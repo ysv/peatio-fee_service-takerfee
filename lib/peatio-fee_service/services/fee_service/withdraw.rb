7bc79e5d492094fbadc0bc435f9d6220db366e85
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-10-11 18:19:40 +0300

    Add documentation

--- lib/peatio-fee_service/services/fee_service/withdraw.rb
+++ lib/peatio-fee_service/services/fee_service/withdraw.rb
@@ -1,16 +1,39 @@
 module Peatio
   class FeeService::Withdraw
-    # This method will be called to calculate fees when we submit Withdraw
-    # inside WithdrawService
+    #
+    # Calculates and returns fees which will be saved and submitted on withdraw submit.
+    #
+    # @abstract Derived Services must implement it.
+    #
+    # @param withdraw [Withdraw]
+    # @return [Fee, [Fee], nil]
+    #   Fee instance, fee instances array or nil.
     def on_submit(withdraw)
       method_not_implemented
     end
 
     #
+    # Calculates and returns fees which will be append to parent withdrawal.
+    # This fees will be submitted and completed automatically on withdraw complete.
+    #
+    # @abstract Derived Services must implement it.
+    #
+    # @param withdraw [Withdraw]
+    # @return [Fee, [Fee], nil]
+    #   Fee instance, fee instances array or nil.
     def on_complete(withdraw)
       method_not_implemented
     end
 
+    #
+    # Calculates and returns fees which will be append to parent withdrawal.
+    # This fees will be submitted and completed automatically on withdraw cancel.
+    #
+    # @abstract Derived Services must implement it.
+    #
+    # @param withdraw [Withdraw]
+    # @return [Fee, [Fee], nil]
+    #   Fee instance, fee instances array or nil.
     def on_cancel(withdraw)
       method_not_implemented
     end
