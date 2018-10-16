0379cb053e192e3c0394bff6acd7e81912a71fbe
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-10-16 17:13:57 +0300

    Fix specs

--- spec/peatio-fee_service/takerfee_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Peatio::FeeService::Takerfee do
-  it "has a version number" do
-    expect(Peatio::FeeService::Takerfee::VERSION).not_to be nil
-  end
-
-  xit "does something useful" do
-    expect(false).to eq(true)
-  end
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
-require "peatio/peatio-takerfee"
+require "peatio"
+require "peatio-takerfee"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
